ee4a28bd4b012953a04a1a3761b533420c518ca3
Ty Mick <ty@tymick.me>
2020-09-29 19:06:08 -0400

    Add get_recent_threads (minus replies)

--- api/thread.py
+++ api/thread.py
@@ -1,4 +1,4 @@
-from flask import request, redirect
+from flask import request, redirect, jsonify
 from database import get_db
 from datetime import datetime, timezone
 import nanoid
@@ -10,7 +10,7 @@ def add_new_thread(board_id):
         now_timestamp = datetime.now(timezone.utc).timestamp()
         db.cursor().execute(
             """
-            INSERT INTO Thread(
+            INSERT INTO thread(
                 _id, board_id, text, created_on, bumped_on, reported, delete_password
             )
             VALUES (?, ?, ?, ?, ?, ?, ?)
@@ -27,14 +27,34 @@ def add_new_thread(board_id):
         )
         db.commit()
 
-        return redirect(f"/b/{board_id}/")
+        return redirect(f"/b/{board_id}")
 
     except:
         return {"error": "Database error"}
 
 
 def get_recent_threads(board_id):
-    pass
+    try:
+        db = get_db()
+        c = db.cursor()
+        c.execute(
+            """
+            SELECT
+                _id,
+                text,
+                datetime(created_on, "unixepoch") AS created_on,
+                datetime(bumped_on, "unixepoch") AS bumped_on
+            FROM thread
+            WHERE board_id == ?
+            ORDER BY bumped_on DESC
+            LIMIT 10
+            """,
+            (board_id,),
+        )
+        return jsonify(c.fetchall())
+
+    except:
+        return {"error": "Database error"}
 
 
 def report_thread(board_id):
--- database.py
+++ database.py
@@ -4,10 +4,22 @@ import sqlite3
 DATABASE = "message_board.db"
 
 
+def dict_factory(cursor, row):
+    """
+    Turns rows into dictionaries for easier JSON conversion. Plugs into
+    Connection.row_factory.
+    """
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = dict_factory
     return db
 
 
@@ -17,17 +29,18 @@ def init_db():
 
     c.execute(
         """
-        CREATE TABLE IF NOT EXISTS Thread(
-            _id TEXT UNIQUE NOT NULL,
+        CREATE TABLE IF NOT EXISTS thread(
+            _id TEXT PRIMARY KEY,
             board_id TEXT NOT NULL,
             text TEXT NOT NULL,
             created_on DATETIME NOT NULL,
             bumped_on DATETIME NOT NULL,
             reported BOOLEAN NOT NULL,
             delete_password TEXT NOT NULL
-        )
+        ) WITHOUT ROWID
         """
     )
-    c.execute("CREATE INDEX IF NOT EXISTS idx_board ON Thread(board_id)")
+    threads = c.fetchall()
+    # Create indices
 
     db.commit()
